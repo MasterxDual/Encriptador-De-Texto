c8c420b456c88572b1c267ecf20710532b1145ca
Agustin Brambilla <90217316+MasterxDual@users.noreply.github.com>
2022-01-29 22:51:58 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 function botonEncriptado() {
     const encriptar = document.getElementById("input-texto").value
-    const encriptado = encriptar.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
+    const encriptando = encriptar.replace(/[^a-zA-Z]/g, "");
+    const encriptado = encriptando.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
     document.getElementById("input-texto2").value = encriptado;
 }
 function botonDesencriptado() {
     const desencriptar = document.getElementById("input-texto").value;
-    const desencriptado = desencriptar.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u");
+    const desencriptando = desencriptar.replace(/[^a-zA-Z]/g, "");
+    const desencriptado = desencriptando.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u");
     document.getElementById("input-texto2").value = desencriptado;
 }
 function botonCopiar() {
@@ -16,3 +18,8 @@ function botonCopiar() {
 function botonPegar() {
     document.getElementById("input-texto").value = document.getElementById("input-texto2").value;
 }
+
+//Elimina los acentos: 
+
+//const encriptando2 = encriptando1.normalize("NFD").replace(/\p{Diacritic}/gu, ""); 
+// const desencriptando = desencriptar.normalize("NFD").replace(/\p{Diacritic}/gu, ""); //Elimina los acentos
